efb88db5e561b20e70d71cec78ff8a970df4d728
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-03-06 10:36:36 +0100

    Update HopperMod.java

--- src/main/java/com/hoppermod/HopperMod.java
+++ src/main/java/com/hoppermod/HopperMod.java
@@ -1,99 +1,89 @@
 package com.hoppermod;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.CommandDispatcher;
-
-import net.minecraft.commands.Commands;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.network.chat.Component;
-
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod(HopperMod.MOD_ID)
+@Mod("hoppermod")
 public class HopperMod {
 
     public static final String MOD_ID = "hoppermod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    // Vanilla defaults
+    // Vanilla defaults: cooldown = 8 ticks, amount = 1 item
     public static int hopperSpeed = 8;
     public static int hopperAmount = 1;
 
     public HopperMod() {
-        LOGGER.info("HopperMod initialized!");
-
-        MinecraftForge.EVENT_BUS.register(this);
+        LOGGER.info("HopperMod 1.21.X loaded!");
     }
 
-    @SubscribeEvent
-    public void onRegisterCommands(RegisterCommandsEvent event) {
+    @Mod.EventBusSubscriber(modid = MOD_ID)
+    public static class CommandRegistry {
 
-        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
-
-        // /hopperspeed
-        dispatcher.register(
-            Commands.literal("hopperspeed")
-                .executes(ctx -> {
-                    ctx.getSource().sendSuccess(
-                        () -> Component.literal(
-                            "§6[HopperMod] §eCurrent Hopper Speed: §f" + hopperSpeed +
-                            " §eTick(s) (Vanilla = 8, lower = faster)"
-                        ),
-                        false
-                    );
-                    return 1;
-                })
-                .then(Commands.argument("ticks", IntegerArgumentType.integer(0, 6400))
+        @SubscribeEvent
+        public static void onRegisterCommands(RegisterCommandsEvent event) {
+            event.getDispatcher().register(
+                Commands.literal("hopperspeed")
                     .executes(ctx -> {
-                        int ticks = IntegerArgumentType.getInteger(ctx, "ticks");
-                        hopperSpeed = ticks;
-
                         ctx.getSource().sendSuccess(
-                            () -> Component.literal(
-                                "§6[HopperMod] §aHopper speed set to §f" + ticks +
-                                " §aTick(s) per transfer. §7(Vanilla = 8)"
+                            Component.literal(
+                                "[HopperMod] Current hopper speed: " + hopperSpeed +
+                                " tick(s) (Vanilla = 8, lower = faster)"
                             ),
                             false
                         );
                         return 1;
                     })
-                )
-        );
+                    .then(Commands.argument("ticks", IntegerArgumentType.integer(0, 6400))
+                        .executes(ctx -> {
+                            int ticks = IntegerArgumentType.getInteger(ctx, "ticks");
+                            hopperSpeed = ticks;
+                            ctx.getSource().sendSuccess(
+                                Component.literal(
+                                    "[HopperMod] Hopper speed set to " + ticks +
+                                    " tick(s) per transfer (Vanilla = 8)"
+                                ),
+                                false
+                            );
+                            return 1;
+                        })
+                    )
+            );
 
-        // /hopperamount
-        dispatcher.register(
-            Commands.literal("hopperamount")
-                .executes(ctx -> {
-                    ctx.getSource().sendSuccess(
-                        () -> Component.literal(
-                            "§6[HopperMod] §eCurrent Hopper Amount: §f" + hopperAmount +
-                            " §eItem(s) per transfer (Vanilla = 1)"
-                        ),
-                        false
-                    );
-                    return 1;
-                })
-                .then(Commands.argument("amount", IntegerArgumentType.integer(1, 9999))
+            event.getDispatcher().register(
+                Commands.literal("hopperamount")
                     .executes(ctx -> {
-                        int amount = IntegerArgumentType.getInteger(ctx, "amount");
-                        hopperAmount = amount;
-
                         ctx.getSource().sendSuccess(
-                            () -> Component.literal(
-                                "§6[HopperMod] §aHopper amount set to §f" + amount +
-                                " §aItem(s) per transfer. §7(Vanilla = 1)"
+                            Component.literal(
+                                "[HopperMod] Current hopper amount: " + hopperAmount +
+                                " item(s) per transfer (Vanilla = 1)"
                             ),
                             false
                         );
                         return 1;
                     })
-                )
-        );
+                    .then(Commands.argument("amount", IntegerArgumentType.integer(1, 64))
+                        .executes(ctx -> {
+                            int amount = IntegerArgumentType.getInteger(ctx, "amount");
+                            hopperAmount = amount;
+                            ctx.getSource().sendSuccess(
+                                Component.literal(
+                                    "[HopperMod] Hopper amount set to " + amount +
+                                    " item(s) per transfer (Vanilla = 1)"
+                                ),
+                                false
+                            );
+                            return 1;
+                        })
+                    )
+            );
+        }
     }
 }
