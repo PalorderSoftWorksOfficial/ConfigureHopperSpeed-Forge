77b6765bd86ba07b787817e25747681ef36b45a8
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2026-03-06 10:26:43 +0100

    Update HopperMod.java

--- src/main/java/com/hoppermod/HopperMod.java
+++ src/main/java/com/hoppermod/HopperMod.java
@@ -1,85 +1,99 @@
 package com.hoppermod;
 
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.text.Text;
+import com.mojang.brigadier.CommandDispatcher;
+
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class HopperMod implements ModInitializer {
+@Mod(HopperMod.MOD_ID)
+public class HopperMod {
 
     public static final String MOD_ID = "hoppermod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    // Vanilla defaults: cooldown = 8 ticks, amount = 1 item
+    // Vanilla defaults
     public static int hopperSpeed = 8;
     public static int hopperAmount = 1;
 
-    @Override
-    public void onInitialize() {
+    public HopperMod() {
         LOGGER.info("HopperMod initialized!");
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
 
-            // /hopperspeed [ticks]
-            dispatcher.register(
-                CommandManager.literal("hopperspeed")
+    @SubscribeEvent
+    public void onRegisterCommands(RegisterCommandsEvent event) {
+
+        CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+
+        // /hopperspeed
+        dispatcher.register(
+            Commands.literal("hopperspeed")
+                .executes(ctx -> {
+                    ctx.getSource().sendSuccess(
+                        () -> Component.literal(
+                            "§6[HopperMod] §eCurrent Hopper Speed: §f" + hopperSpeed +
+                            " §eTick(s) (Vanilla = 8, lower = faster)"
+                        ),
+                        false
+                    );
+                    return 1;
+                })
+                .then(Commands.argument("ticks", IntegerArgumentType.integer(0, 6400))
                     .executes(ctx -> {
-                        ctx.getSource().sendFeedback(
-                            () -> Text.literal(
-                                "§6[HopperMod] §eAktuelle Hopper-Geschwindigkeit: §f" + hopperSpeed +
-                                " §eTick(s) (Vanilla = 8, niedriger = schneller)"
+                        int ticks = IntegerArgumentType.getInteger(ctx, "ticks");
+                        hopperSpeed = ticks;
+
+                        ctx.getSource().sendSuccess(
+                            () -> Component.literal(
+                                "§6[HopperMod] §aHopper speed set to §f" + ticks +
+                                " §aTick(s) per transfer. §7(Vanilla = 8)"
                             ),
                             false
                         );
                         return 1;
                     })
-                    .then(CommandManager.argument("ticks", IntegerArgumentType.integer(1, 1200))
-                        .executes(ctx -> {
-                            int ticks = IntegerArgumentType.getInteger(ctx, "ticks");
-                            hopperSpeed = ticks;
-                            ctx.getSource().sendFeedback(
-                                () -> Text.literal(
-                                    "§6[HopperMod] §aHopper-Geschwindigkeit gesetzt auf §f" + ticks +
-                                    " §aTick(s) pro Transfer. §7(Vanilla = 8)"
-                                ),
-                                false
-                            );
-                            return 1;
-                        })
-                    )
-            );
+                )
+        );
 
-            // /hopperamount [items]
-            dispatcher.register(
-                CommandManager.literal("hopperamount")
+        // /hopperamount
+        dispatcher.register(
+            Commands.literal("hopperamount")
+                .executes(ctx -> {
+                    ctx.getSource().sendSuccess(
+                        () -> Component.literal(
+                            "§6[HopperMod] §eCurrent Hopper Amount: §f" + hopperAmount +
+                            " §eItem(s) per transfer (Vanilla = 1)"
+                        ),
+                        false
+                    );
+                    return 1;
+                })
+                .then(Commands.argument("amount", IntegerArgumentType.integer(1, 64))
                     .executes(ctx -> {
-                        ctx.getSource().sendFeedback(
-                            () -> Text.literal(
-                                "§6[HopperMod] §eAktuelle Hopper-Item-Menge: §f" + hopperAmount +
-                                " §eItem(s) pro Transfer (Vanilla = 1)"
+                        int amount = IntegerArgumentType.getInteger(ctx, "amount");
+                        hopperAmount = amount;
+
+                        ctx.getSource().sendSuccess(
+                            () -> Component.literal(
+                                "§6[HopperMod] §aHopper amount set to §f" + amount +
+                                " §aItem(s) per transfer. §7(Vanilla = 1)"
                             ),
                             false
                         );
                         return 1;
                     })
-                    .then(CommandManager.argument("amount", IntegerArgumentType.integer(1, 64))
-                        .executes(ctx -> {
-                            int amount = IntegerArgumentType.getInteger(ctx, "amount");
-                            hopperAmount = amount;
-                            ctx.getSource().sendFeedback(
-                                () -> Text.literal(
-                                    "§6[HopperMod] §aHopper-Item-Menge gesetzt auf §f" + amount +
-                                    " §aItem(s) pro Transfer. §7(Vanilla = 1)"
-                                ),
-                                false
-                            );
-                            return 1;
-                        })
-                    )
-            );
-        });
+                )
+        );
     }
 }
